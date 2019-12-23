4f2bc0bdddd1c607efe964d00bff42444dc9b268
waj949 <wajdihamrouni1993@gmail.com>
2019-12-23 20:25:50 +0100

    lol

--- database/index.js
+++ database/index.js
@@ -1,16 +1,24 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/fetcher", { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("connected hhhhhhhhhh");
+});
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  name: String,
+  fork: Boolean
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+let Repo = mongoose.model("Repo", repoSchema);
 
 let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-}
+  // i dnt need it
+};
 
 module.exports.save = save;
+module.exports.Repo = Repo;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("../database/index");
+const Repo = db.Repo;
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + "/../client/dist"));
 
-app.post('/repos', function (req, res) {
+app.post("/repos", function(req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  var repo = new Repo(req.body);
+  repo.save();
+  res.send().status(201);
 });
 
-app.get('/repos', function (req, res) {
+app.get("/repos", function(req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  Repo.find().then(doc => {
+    console.log(doc);
+  });
+  res.send("k");
 });
 
 let port = 1128;
@@ -20,4 +32,3 @@ let port = 1128;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
